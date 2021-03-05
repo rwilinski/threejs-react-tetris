5efffca1692c28efae6663d6ab34f0b65014a5b3
rwilinski <wileeek@gmail.com>
2021-03-05 16:55:27 +0100

    Improved key binding

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -16,132 +16,12 @@ export function Game() {
   const state = useStore();
   const [gameInterval, setGameInterval] = useState<number | null>(null);
 
-  useEventListener("keydown", ({ key }: { key: string }) => {
-    switch (key) {
-      case "ArrowUp":
-        if (!state.currentBlock || state.gameOver || state.gamePaused) {
-          return;
-        }
+  const startNewGame = () => {
+    setGameInterval(NORMAL_SPEED);
 
-        const { path, defaultPath } = state.currentBlock;
-        const offsetLeft = path[0][0] - defaultPath[0][0];
-        const offsetTop = path[0][1] - defaultPath[0][1];
-
-        const next = rotateBlock(state.currentBlock);
-
-        let canRotate = true;
-
-        next.path = next.path.map((item) => [
-          item[0] + offsetLeft,
-          item[1] + offsetTop,
-        ]);
-
-        for (const item of next.path) {
-          if (
-            item[0] < 0 ||
-            item[1] < 0 ||
-            item[0] >= BOARD_WIDTH ||
-            item[1] >= BOARD_HEIGHT ||
-            (state.board[item[1]][item[0]] &&
-              !isCellActive(state.board[item[1]][item[0]]))
-          ) {
-            canRotate = false;
-          }
-        }
-
-        if (canRotate) {
-          state.setCurrentBlock(next);
-        }
-
-        state.updateBoard();
-        break;
-
-      case "ArrowLeft":
-        if (!state.currentBlock || state.gameOver || state.gamePaused) {
-          return;
-        }
-
-        let canBeShifted2 = true;
-
-        state.currentBlock.path.forEach((item) => {
-          const cell = state.board[item[1]][item[0] - 1];
-
-          if (!(item[0] - 1 >= 0) || (cell && !isCellActive(cell))) {
-            canBeShifted2 = false;
-          }
-        });
-
-        if (canBeShifted2) {
-          state.moveLeft();
-          state.updateBoard();
-        }
-        break;
-
-      case "ArrowRight":
-        if (!state.currentBlock || state.gameOver || state.gamePaused) {
-          return;
-        }
-
-        let canBeShifted3 = true;
-
-        state.currentBlock.path.forEach((item) => {
-          const cell = state.board[item[1]][item[0] + 1];
-
-          if (!(item[0] + 1 < BOARD_WIDTH) || (cell && !isCellActive(cell))) {
-            canBeShifted3 = false;
-          }
-        });
-
-        if (canBeShifted3) {
-          state.moveRight();
-          state.updateBoard();
-        }
-        break;
-
-      case "ArrowDown":
-        if (!state.currentBlock || state.gameOver || state.gamePaused) {
-          return;
-        }
-
-        setGameInterval(FAST_SPEED);
-
-        break;
-
-      case " ":
-        if (state.gameOver) {
-          startNewGame();
-
-          return;
-        }
-
-        if (state.gamePaused) {
-          setGameInterval(NORMAL_SPEED);
-        } else {
-          setGameInterval(null);
-        }
-
-        state.toggleGamePause();
-        break;
-
-      default:
-        break;
-    }
-  });
-
-  useEventListener("keyup", ({ key }: { key: string }) => {
-    switch (key) {
-      case "ArrowDown":
-        if (!state.currentBlock || state.gameOver || state.gamePaused) {
-          return;
-        }
-
-        setGameInterval(NORMAL_SPEED);
-
-        break;
-      default:
-        break;
-    }
-  });
+    state.restart();
+    state.updateBoard();
+  };
 
   const canAddNewBlock = () => {
     let canBePlaced = true;
@@ -156,6 +36,131 @@ export function Game() {
 
     return canBePlaced;
   };
+
+  const rotate = () => {
+    const { path, defaultPath } = state.currentBlock;
+    const offsetLeft = path[0][0] - defaultPath[0][0];
+    const offsetTop = path[0][1] - defaultPath[0][1];
+
+    const next = rotateBlock(state.currentBlock);
+
+    let canRotate = true;
+
+    next.path = next.path.map((item) => [
+      item[0] + offsetLeft,
+      item[1] + offsetTop,
+    ]);
+
+    for (const item of next.path) {
+      if (
+        item[0] < 0 ||
+        item[1] < 0 ||
+        item[0] >= BOARD_WIDTH ||
+        item[1] >= BOARD_HEIGHT ||
+        (state.board[item[1]][item[0]] &&
+          !isCellActive(state.board[item[1]][item[0]]))
+      ) {
+        canRotate = false;
+      }
+    }
+
+    if (canRotate) {
+      state.setCurrentBlock(next);
+    }
+
+    state.updateBoard();
+  };
+
+  const moveLeft = () => {
+    let canMove = true;
+
+    state.currentBlock.path.forEach((item) => {
+      const cell = state.board[item[1]][item[0] - 1];
+
+      if (!(item[0] - 1 >= 0) || (cell && !isCellActive(cell))) {
+        canMove = false;
+      }
+    });
+
+    if (canMove) {
+      state.moveLeft();
+      state.updateBoard();
+    }
+  };
+
+  const moveRight = () => {
+    let canMove = true;
+
+    state.currentBlock.path.forEach((item) => {
+      const cell = state.board[item[1]][item[0] + 1];
+
+      if (!(item[0] + 1 < BOARD_WIDTH) || (cell && !isCellActive(cell))) {
+        canMove = false;
+      }
+    });
+
+    if (canMove) {
+      state.moveRight();
+      state.updateBoard();
+    }
+  };
+
+  const togglePause = () => {
+    if (state.gameOver) {
+      startNewGame();
+
+      return;
+    }
+
+    setGameInterval(state.gamePaused ? NORMAL_SPEED : null);
+
+    state.toggleGamePause();
+  };
+
+  useEventListener("keydown", ({ key }: { key: string }) => {
+    switch (key) {
+      case "ArrowUp":
+        if (gameInterval) {
+          rotate();
+        }
+
+        break;
+
+      case "ArrowLeft":
+        if (gameInterval) {
+          moveLeft();
+        }
+
+        break;
+
+      case "ArrowRight":
+        if (gameInterval) {
+          moveRight();
+        }
+
+        break;
+
+      case "ArrowDown":
+        if (gameInterval) {
+          setGameInterval(FAST_SPEED);
+        }
+
+        break;
+
+      case " ":
+        togglePause();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  useEventListener("keyup", ({ key }: { key: string }) => {
+    if (key === "ArrowDown" && gameInterval) {
+      setGameInterval(NORMAL_SPEED);
+    }
+  });
 
   useInterval(
     () => {
@@ -199,13 +204,6 @@ export function Game() {
     gameInterval,
     { immediate: false }
   );
-
-  const startNewGame = () => {
-    setGameInterval(NORMAL_SPEED);
-
-    state.restart();
-    state.updateBoard();
-  };
 
   useEffect(() => {
     startNewGame();
