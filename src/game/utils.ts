28ec21d40f17b2deea3fae03204a98031f2fe921
rwilinski <wileeek@gmail.com>
2021-03-13 00:33:33 +0100

    Type

--- src/game/utils.ts
+++ src/game/utils.ts
@@ -1,4 +1,4 @@
-export function getRandomElement<T = any>(arr: T[]): T {
+export function getRandomElement<T = unknown>(arr: T[]): T {
   const randomIndex = Math.floor(Math.random() * arr.length);
 
   return arr[randomIndex];
