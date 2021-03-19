5f91dc803ba87e6d8818e6895744ed6b389a37b8
rwilinski <wileeek@gmail.com>
2021-03-19 14:41:49 +0100

    Change text font and color; remove unused

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useEventListener, useInterval } from "ahooks";
 
 import { useStore } from "../game/store";
--- src/components/RenderText.tsx
+++ src/components/RenderText.tsx
@@ -5,7 +5,13 @@ import { FONT } from "../game/constants";
 
 export function RenderText({ children, ...props }: any) {
   return (
-    <Text color="#111" fontSize={18} font={FONT} textAlign="center" {...props}>
+    <Text
+      color="#191919"
+      fontSize={18}
+      font={FONT}
+      textAlign="center"
+      {...props}
+    >
       {children}
     </Text>
   );
